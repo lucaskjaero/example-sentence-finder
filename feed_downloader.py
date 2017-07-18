f8d99a616c299f669ae726a1afead6e9a7e4e2ee
Lucas Kjaero <lucas@lucaskjaero.com>
2017-07-18 03:16:40 -0700

    NYTimes article reader
    Still need sentences

--- feed_downloader.py
+++ feed_downloader.py
@@ -1,19 +1,60 @@
 import feedparser
+import requests
+
+from bs4 import BeautifulSoup
+
+from feed_reader import FeedReader
 
 NY_TIMES_URL = "https://cn.nytimes.com/rss.html"
 
+class NYTimes(FeedReader):
+    def get_sentences(self):
+        raise NotImplementedError
 
-def download_data():
+    def get_articles(self):
+        articles = []
+        for title, link in download_feed_data():
+            article = get_article_text(link)
+            output_dict = {
+                'title': title,
+                'link': link,
+                'article': article
+            }
+            articles.append(output_dict)
+
+        return articles
+
+
+def download_feed_data():
     feed = feedparser.parse(NY_TIMES_URL)
     data = [[article.title, article.link] for article in feed['entries']]
     return data
 
 
-def main():
-    with open("article_headers.csv", "a") as output:
-        for title, link in download_data():
+def get_article_text(link):
+    html = requests.get(link).text
+    page = BeautifulSoup(html, "html5lib")
+
+    content_div = page.find(id="content")
+    sentences = [paragraph.string for paragraph in content_div.find_all('p', attrs={'class':'paragraph'}) if paragraph.string is not None]
+    article = "".join(sentences)
+
+    return article
+
+
+def write_feed_information(file_name="article_headers.csv"):
+    with open(file_name, "a") as output:
+        for title, link in download_feed_data():
             output_line = '"%s","%s"\n' % (title, link)
             output.write(output_line)
+
+
+def main():
+    write_feed_information()
+    for title, link in download_feed_data():
+        file_name = title + ".txt"
+        with open(file_name, "w") as output:
+            output.write(get_article_text(link))
 
 
 if __name__ == '__main__':
